90dc747bfe151f0ba55f3706635c55b8ff772814
MelissaHernandez94 <melissa.hernandez1@nhs.net>
2026-06-09 11:03:47 +0100

    TKT 2: added data

---
+++ read-data.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df = pd.read_excel("/Users/melissahernandez/dev/test-repo/test-repo/how-often-brits-socialize-with-friends-family-and-hobbies.csv")
+
+print(df.head())
+print(df.shape)
